c563de25e325f5e3dbc0a85a7100524c78c48c93
N-Sam <nyekosamueltreg@gmail.com>
2019-06-08 13:16:59 +0300

    refreshed app

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -134,3 +134,6 @@ STATICFILES_DIRS = [
     '/static/mywebsite/',
     '/static/blog/'
 ]
+#activation of django heruko
+
+django_heroku.settings(local())
