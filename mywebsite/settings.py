005f48cd4efd6e64675fd0b50f3e5a4f586da00f
N-Sam <nyekosamueltreg@gmail.com>
2019-06-08 13:54:19 +0300

    gh

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -129,11 +129,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/static/mywebsite/',
-    '/static/blog/'
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/static/mywebsite/',
+#     '/static/blog/'
+# ]
 #activation of django heruko
 
 django_heroku.settings(local())
